29e95550803694a937d108d766279e728a9436bc
Mandeep Duhan <imandeepduhan@gmail.com>
2025-11-11 09:07:37 +0530

    Update comments in 02_comments.sh
    Fix multi-line comment syntax and update example.

---
+++ 02_comments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script demonstrates single and multi-line comments
+
+echo "Checking Comments"
+
+# This is a single-line comment
+
+<<comment
+This is a multi-line
+Comments
+comment
