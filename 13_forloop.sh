96d7aa953ed97000ed6665d174e41755056b10ef
Mandeep Duhan <imandeepduhan@gmail.com>
2025-11-21 13:29:26 +0530

    Create 13_forloop.sh

---
+++ 13_forloop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Simple for loop
+for i in 1 2 3
+do
+    echo "Number is : $i"
+done
+
+# Loop through names
+for name in Mandeep Kapil Sahil
+do
+    echo "Name is : $name"
+done
+
+# Another for loop using range
+for n in {1..3}
+do
+    echo "Number is : $n"
+done
